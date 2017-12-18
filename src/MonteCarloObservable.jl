cdf73e7927b00a0cd04bcd12315344f65f58bbbb
Carsten Bauer <crstnbr@gmail.com>
2017-12-18 22:26:32 +0100

    basically copied from peter

--- src/MonteCarloObservable.jl
+++ src/MonteCarloObservable.jl
@@ -1,4 +1,19 @@
 module MonteCarloObservable
 
+    using HDF5
 
-end # module
+    include("type.jl")
+    include("statistics.jl")
+    include("io.jl")
+
+    export monte_carlo_observable
+    export integrated_autocorrelation_time
+    export binning_error
+    export jackknife_error
+
+    function Base.start(mco::monte_carlo_observable) state = 1 end
+    function Base.done(mco::monte_carlo_observable, state::Int) return state == mco.curr_bin end
+    function Base.next(mco::monte_carlo_observable, state::Int)
+        return mco.bins[mco.colons..., state], state + 1
+    end
+end
---
+++ src/type.jl
@@ -0,0 +1,17 @@
+type monte_carlo_observable{T}
+    name::String
+    n_measurements::Int
+    keep_timeseries::Int
+    last_dim::Int
+    entry_dims::Array{Int, 1}
+    entry_size::Int
+    timeseries::Array{T}
+    measurement_buffer::Array{T}
+    bins::Array{T}
+    bin_variance_series::Array{T}
+    curr_bin::Int
+    autocorrelation_buffer::Array{T}
+    colons::Array{Colon}
+
+    monte_carlo_observable(name::String, entry_dims::Array{Int, 1}=[1]) = new(name, 1, false, size(entry_dims, 1) + 1, entry_dims, prod(entry_dims), typemin(T) * ones(entry_dims..., 0), typemin(T) * ones(entry_dims..., 1), typemin(T) * ones(entry_dims..., 2^8), typemin(T) * ones(T, entry_dims..., 16), 1, typemin(T) * ones(T, entry_dims..., 2^10), [Colon() for _ in entry_dims])
+end
