6bd1070c18ec60b5d0f88cac73f7d3047c72835b
Carsten Bauer <crstnbr@gmail.com>
2018-11-14 21:05:50 +0100

    doc build config

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,11 @@
 using Documenter, MonteCarloObservable
 
-makedocs(modules = [MonteCarloObservable], doctest=false)
+makedocs(modules = [MonteCarloObservable], doctest=false, format = :markdown)
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math", 
         "pygments", "pymdown-extensions"),
     repo   = "github.com/crstnbr/MonteCarloObservable.jl.git",
-    julia  = "release",
-    osname = "linux",
+    target = "site",
+    make   = () -> run(`mkdocs build`)
 )
